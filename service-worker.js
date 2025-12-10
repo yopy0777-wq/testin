307fade7380bd5b4da069c550ef7cb0d759ef8e6
Hirano <hirano.yoichi@hiroshima-parker.co.jp>
2025-12-10 14:25:56 +0900

    座標変換処理追加

--- service-worker.js
+++ service-worker.js
@@ -60,7 +60,11 @@ self.addEventListener('fetch', event => {
         const responseToCache = response.clone();
         caches.open(CACHE_NAME)
           .then(cache => {
-            cache.put(event.request, responseToCache);
+           // 修正点: URLスキームが http または https であるかを確認
+            if (event.request.url.startsWith('http')) {
+                cache.put(event.request, responseToCache);
+            }
+           // cache.put(event.request, responseToCache);
           });
 
         return response;
